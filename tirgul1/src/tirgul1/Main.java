4c3e518c60c427eb9a4221acc384813942c506d5
whisper120 <84220690+whisper120@users.noreply.github.com>
2024-01-24 22:32:54 +0200

    Update Main.java
    added comment

--- tirgul1/src/tirgul1/Main.java
+++ tirgul1/src/tirgul1/Main.java
@@ -3,7 +3,7 @@ package tirgul1;
 public class Main {
 
 	public static void main(String[] args) {
-		//a test is here
+		//a test is here ,and another one
 		Timer t1 = new Timer(10, 0);
 		Timer t2 = new Timer(200);
 		Timer t3 = new Timer(0);
