f4de28899361e78f2469c3c69d76f9592b2632cd
whisper120 <84220690+whisper120@users.noreply.github.com>
2024-01-24 21:12:48 +0200

    added a comment

--- tirgul1/src/tirgul1/Main.java
+++ tirgul1/src/tirgul1/Main.java
@@ -3,6 +3,7 @@ package tirgul1;
 public class Main {
 
 	public static void main(String[] args) {
+		//a test is here
 		Timer t1 = new Timer(10, 0);
 		Timer t2 = new Timer(200);
 		Timer t3 = new Timer(0);
@@ -12,7 +13,7 @@ public class Main {
 
 		Timer t4;
 		t4 = t1.diff(t1);
-		t4.printInfo();
+		t4.printInfo();W
 	}
 
 }
